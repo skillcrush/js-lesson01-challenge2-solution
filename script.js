73de2639f8536aefba05015e74472ae67fa42703
tauri_s <tauristclaire@gmail.com>
2026-09-22 17:51:50 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Write your JavaScript below this line
+var firstName = "Ann";
+var favoriteSnack = "pie";
+
+console.log(
+  "Hi, my name is " +
+    firstName +
+    " and I wish I had " +
+    favoriteSnack +
+    " right now."
+);
